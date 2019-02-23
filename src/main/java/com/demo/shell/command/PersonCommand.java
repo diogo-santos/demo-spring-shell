dcfd65b8a7e21823a36590e414e50f90493d2b22
Diogo Santos <diogoffs@gmail.com>
2019-02-23 00:29:10 +0000

    Some refactoring and coverage

--- src/main/java/com/demo/shell/command/PersonCommand.java
+++ src/main/java/com/demo/shell/command/PersonCommand.java
@@ -1,6 +1,8 @@
 package com.demo.shell.command;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.util.List;
 import java.util.Optional;
@@ -31,7 +33,7 @@ public class PersonCommand {
     @ShellMethod("Add a person [firstName, surname] returns id")
     long add(String firstName, String surname) {
         logger.debug("In add with firstName={} surname={}", firstName, surname);
-    	Person personSaved = repository.save(Person.builder()
+    	Person personSaved = this.repository.save(Person.builder()
 													.firstName(firstName)
 													.surname(surname)
 													.build());
@@ -44,12 +46,12 @@ public class PersonCommand {
     String edit(long id, String firstName, String surname) {
         logger.debug("In edit with id={} firstName={} surname={}", id, firstName, surname);
     	String message = String.format("Person id %s does not exist", id);
-		Optional<Person> personOpt = repository.findById(id);
+		Optional<Person> personOpt = this.repository.findById(id);
         if (personOpt.isPresent()) {
         	Person person = personOpt.get();
         	person.setFirstName(firstName);
 			person.setSurname(surname);
-			repository.save(person);
+            this.repository.save(person);
 			message = "Edit success";
 		}
         logger.debug("Out edit with {} ", message);
@@ -61,7 +63,7 @@ public class PersonCommand {
         logger.debug("In delete with {} ", id);
     	String message = "Delete success";
         try {
-        	repository.deleteById(id);
+            this.repository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
 			message = String.format("Person id %s does not exist", id);
 		}
@@ -71,14 +73,14 @@ public class PersonCommand {
     
     @ShellMethod("Count Number of Persons")
     long count() {
-        long count = repository.count();
+        long count = this.repository.count();
         logger.debug("Out count with {} ", count);
         return count;
     }
     
     @ShellMethod("List Persons")
     List<Person> list() {
-	    List<Person> list = repository.findAll();
+	    List<Person> list = this.repository.findAll();
         logger.debug("Out list with {} ", list.size());
         return list;
     }
@@ -86,14 +88,11 @@ public class PersonCommand {
     @ShellMethod("Add a person from xml file [xmlFilePath] - windows path eg. c:/dir/file.xml, xml content eg. <person><firstName></firstName><surname></surname></person>")
     long addXmlFile(String xmlFilePath) {
         logger.debug("In addXmlFile with {} ", xmlFilePath);
-        File xmlFile = new File(xmlFilePath);
         long result = 0;
         try {
-            JAXBContext jaxbContext;
-            jaxbContext = JAXBContext.newInstance(Person.class);
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            Person person = (Person) jaxbUnmarshaller.unmarshal(xmlFile);
-            Person personSaved = repository.save(person);
+            Unmarshaller jaxbUnmarshaller = JAXBContext.newInstance(Person.class).createUnmarshaller();
+            Person person = (Person) jaxbUnmarshaller.unmarshal(this.newFileInputStream(xmlFilePath));
+            Person personSaved = this.repository.save(person);
             result = personSaved.getId();
         } catch (Exception e) {
             logger.debug("Error addXmlFile with {} ", xmlFilePath);
@@ -103,16 +102,18 @@ public class PersonCommand {
         return result;
     }
 
+    InputStream newFileInputStream(String path) throws Exception {
+	    return new FileInputStream(new File(path));
+    }
+
     @ShellMethod("Add a person from xml text [xmlText] - eg. <person><firstName>name</firstName><surname>surname</surname></person>")
     long addXmlText(String xmlText) {
         logger.debug("In addXmlText with {} ", xmlText);
         long result = 0;
         try {
-            JAXBContext jaxbContext;
-            jaxbContext = JAXBContext.newInstance(Person.class);
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Unmarshaller jaxbUnmarshaller = JAXBContext.newInstance(Person.class).createUnmarshaller();
             Person person = (Person) jaxbUnmarshaller.unmarshal(new StringReader(xmlText));
-            Person personSaved = repository.save(person);
+            Person personSaved = this.repository.save(person);
             result = personSaved.getId();
         } catch (Exception e) {
             logger.debug("Error addXmlText with {} ", xmlText);
--- src/test/java/com/demo/shell/command/PersonCommandTest.java
+++ src/test/java/com/demo/shell/command/PersonCommandTest.java
@@ -1,97 +1,113 @@
 package com.demo.shell.command;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.shell.jline.InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED;
+import static org.springframework.shell.jline.ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.shell.jline.InteractiveShellApplicationRunner;
-import org.springframework.shell.jline.ScriptShellApplicationRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
 import com.demo.shell.entity.Person;
 import com.demo.shell.repo.PersonRepository;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(properties = {InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
-	    						ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"})
+@SpringBootTest(properties = {SPRING_SHELL_INTERACTIVE_ENABLED+"=false",SPRING_SHELL_SCRIPT_ENABLED+"=false"})
 public class PersonCommandTest {
 	@Autowired
 	private PersonRepository repository;
 	@Autowired
 	private PersonCommand cmd;
 
-	@Before
-	public void setup() {
-		cmd = new PersonCommand(repository);
-	}
-
 	@Test
 	public void addTest() {
-		long result = cmd.add("firstAdd", "lastAdd");
-		Assert.isInstanceOf(Long.class, result);
-		List<Person> personList = repository.findAll();
+		long result = this.cmd.add("firstAdd", "lastAdd");
+		List<Person> personList = this.repository.findAll();
 		assertThat(personList).extracting(Person::getId).contains(result);
 		assertThat(personList).extracting(Person::getFirstName).contains("firstAdd");
 		assertThat(personList).extracting(Person::getSurname).contains("lastAdd");
-		
-		repository.deleteById(result);
 	}
 	
 	@Test
 	public void editTest() {
-		Long id = repository.save(Person.builder().firstName("test").surname("test").build())
-							.getId();
-		cmd.edit(id, "firstEdit", "lastEdit");
-		List<Person> personList = repository.findAll();
+		long id = this.repository.save(Person.builder()
+										.firstName("test")
+										.surname("test").build())
+								 .getId();
+		this.cmd.edit(id, "firstEdit", "lastEdit");
+		List<Person> personList = this.repository.findAll();
 		assertThat(personList).extracting(Person::getId).contains(id);
 		assertThat(personList).extracting(Person::getFirstName).contains("firstEdit");
 		assertThat(personList).extracting(Person::getSurname).contains("lastEdit");
-		
-		repository.deleteById(id);
 	}
 	
 	@Test
 	public void deleteTest() {
-		Long id = repository.save(Person.builder().firstName("testDelete").surname("testDelete").build())
-							.getId();
-		cmd.delete(id);
-		List<Person> personList = repository.findAll();
+		long id = this.repository.save(Person.builder()
+										.firstName("testDelete")
+										.surname("testDelete").build())
+								 .getId();
+		this.cmd.delete(id);
+		List<Person> personList = this.repository.findAll();
 		assertThat(personList).extracting(Person::getId).doesNotContain(id);
 	}
 	
 	@Test
 	public void countTest() {
-		repository.save(Person.builder().build());
-		long count = cmd.count();
-		assertThat(count == 1);
+		long count = this.cmd.count();
+		assertEquals(repository.count() , count);
 	}
 	
 	@Test
 	public void listTest() {
-		repository.save(Person.builder().firstName("testList").surname("testList").build());
-		List<Person> personList = cmd.list();
-		assertThat(personList).extracting(Person::getFirstName).contains("testList");
-		assertThat(personList).extracting(Person::getSurname).contains("testList");
+		this.repository.save(Person.builder().firstName("firstList1").surname("lastList1").build());
+		this.cmd.add("firstList2","lastList2");
+		List<Person> personList = this.cmd.list();
+		assertThat(personList).extracting(Person::getFirstName).contains("firstList1","firstList2");
+		assertThat(personList).extracting(Person::getSurname).contains("lastList1","lastList2");
 	}
 
 	@Test
 	public void editNotExistTest() {
-		String result = cmd.edit(-1000000, "test", "test");
-		assertFalse("Edit success".equals(result));
+		String result = this.cmd.edit(-1000000, "test", "test");
+		assertNotEquals("Edit success", result);
 	}
 
 	@Test
 	public void deleteNotExistTest() {
-		String result = cmd.delete(-1000000);
-		assertFalse("Delete success".equals(result));
+		String result = this.cmd.delete(-1000000);
+		assertNotEquals("Delete success", result);
+	}
+
+	@Test
+	public void addXmlFileTest() throws Exception {
+		String xml = "<person>" +
+						"<firstName>firstXmlFile</firstName>" +
+						"<surname>lastXmlFile</surname>" +
+					 "</person>";
+		InputStream is = new ByteArrayInputStream(xml.getBytes());
+
+		PersonCommand cmdSpy = Mockito.spy(this.cmd);
+		doReturn(is)
+				.when(cmdSpy)
+				.newFileInputStream(anyString());
+
+		long id = cmdSpy.addXmlFile("person.xml");
+
+		List<Person> personList = this.repository.findAll();
+		assertThat(personList).extracting(Person::getId).contains(id);
+		assertThat(personList).extracting(Person::getFirstName).contains("firstXmlFile");
+		assertThat(personList).extracting(Person::getSurname).contains("lastXmlFile");
 	}
 
 	@Test
@@ -100,28 +116,26 @@ public class PersonCommandTest {
 						"<firstName>firstXml</firstName>" +
 						"<surname>lastXml</surname>" +
 					 "</person>";
-		long id = cmd.addXmlText(xml);
+		long id = this.cmd.addXmlText(xml);
 
-		List<Person> personList = repository.findAll();
+		List<Person> personList = this.repository.findAll();
 		assertThat(personList).extracting(Person::getId).contains(id);
 		assertThat(personList).extracting(Person::getFirstName).contains("firstXml");
 		assertThat(personList).extracting(Person::getSurname).contains("lastXml");
-
-		repository.deleteById(id);
 	}
 
 	@Test
-	public void addXmlTextErrorTest() {
+	public void addXmlTextWhenErrorTest() {
 		String xml = "<person>" +
 						"<firstName>firstXml" +
 						"<surname>lastXml";
-		long id = cmd.addXmlText(xml);
+		long id = this.cmd.addXmlText(xml);
 		assertEquals(0, id);
 	}
 
 	@Test
-	public void addXmlFileNotExistsTest() {
-		long id = cmd.addXmlFile("");
+	public void addXmlWhenFileNotExistsTest() {
+		long id = this.cmd.addXmlFile("");
 		assertEquals(0, id);
 	}
 }
